696eb993aad35b85bac2781981bd2c1399f070ad
nathan-j-brenner <nathan.j.brenner@gmail.com>
2015-06-24 14:57:52 -0700

    stopped working on collection, html isn't rendering the collection, need to look again at the code above it

--- calendar.js
+++ calendar.js
@@ -1,7 +1,11 @@
 //model for day
 var DayModel = Backbone.Model.extend({
-	defaults : {"value" : 0} //value will represent the date of a month
+	defaults : {"value" : 0}, //value will represent the date of a month
+	replace : function(number){
+		this.set("value" : number);
+	}
 });
+
 //view for day
 var DayView = Backbone.View.extend({
 	render	: function(){
@@ -11,8 +15,38 @@ var DayView = Backbone.View.extend({
 		console.log('render completed');
 	}
 });
-//collection for days
 
+//collection for days
+var DayCollection = Backbone.Collection.extend({
+	model : DayModel
+});
+
+//view for day collection
+var DayCollectionView = Backbone.View.extend({
+	render : function(){
+		var add_day_btn = '<button id="add_day">Add</button>';
+		var day_div = ''
+		this.$el.html(add_day_btn);
+	},
+	initialize: function(){
+		this.listenTo(this.collection, 'add', this.add_day_view);
+		//listenTo(collection, event, callback)
+	},
+	events : {
+		'click #add_day' : 'add_day'
+	},
+	add_day_model : function(){
+		this.collection.add({});
+	},
+	add_day_view : function(){
+		var default_day_value = this.model.get("value");
+		var day_value = default_day_value + 1;
+		new_day_model.set("value", day_value);
+		var view = new DayView({model: new_day_model});
+		view.render();
+		this.$("#calendarDiv").append(view.$el);
+	}
+})
 
 var day_model1, day_view1;
 $(document).ready(function(){
