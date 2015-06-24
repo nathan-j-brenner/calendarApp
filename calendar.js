19d9947fe75f2a339e31ce21d28e02d9db0a31c6
nathan-j-brenner <nathan.j.brenner@gmail.com>
2015-06-24 14:23:51 -0700

    day_view renders value in html

--- calendar.js
+++ calendar.js
@@ -1,9 +1,22 @@
 //model for day
 var DayModel = Backbone.Model.extend({
-	defaults : {"value" : 0}
+	defaults : {"value" : 1} //value will represent the date of a month
 });
 //view for day
+var DayView = Backbone.View.extend({
+	render	: function(){
+		var day_value = this.model.get("value");
+		this.$el.html('<p>' + day_value + '</p>');
+		console.log('render completed');
+	}
+});
 //collection for days
-$(document).ready(function(){
 
+
+var day_model1, day_view1;
+$(document).ready(function(){
+	day_model1 = new DayModel();
+	day_view1 = new DayView({model: day_model1});
+	day_view1.render();
+	$("#calendarDiv").append(day_view1.$el);
 });
