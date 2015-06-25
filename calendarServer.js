1f3998a96209a56ff2e93f4245a1ea172ea95bed
nathan-j-brenner <nathan.j.brenner@gmail.com>
2015-06-24 17:54:20 -0700

    server ready...until I run it and it breaks

--- calendarServer.js
+++ calendarServer.js
@@ -9,18 +9,29 @@ app.use(express.static(__dirname));
 
 var dates = [];
 
-//get: 
-app.get('/dates/1', function(req, res){
+//get: fetch from the Day_model, return the JSON object that packs up the value property from the dates array on the server
+app.get('/dates/:id', function(req, res){
 	console.log("date has been requested")
-	res.send(JSON.stringify({value: dates}));
+	var id = req.params.id;
+	res.send(JSON.stringify({value: dates[id]}));
 });
 
-//put
-app.put('/dates/1', function(req, res){
+//put: used when we modify the Day_model
+app.put('/dates/:id', function(req, res){
 	console.log(req.body);
-	dates = req.body.value;
-	res.end(JSON.stringify({}));
+	var id = req.params.id;
+	dates[id] = req.body.value;
+	res.end();
 });
+
+//get: used to initialize the data for the Day_collection, and pack up the array of objects to feed into the collection and send it
+app.get('/dates', function(req, res){
+	var dates_and_ids = dates.map(function(v, i){
+		return {id : i, value : v};
+	});
+	res.send(dates_and_ids);
+});
+
 
 app.listen(3000, function(){
 	console.log("server started");
