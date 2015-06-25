b1eaa25d08853f06e096517fb2a1068730c01dc1
nathan-j-brenner <nathan.j.brenner@gmail.com>
2015-06-25 13:56:37 -0700

    somehow fixed the bug where the day wasn't incrementing and showing up on the screen

--- calendar.js
+++ calendar.js
@@ -1,6 +1,6 @@
 //model for day
 var DayModel = Backbone.Model.extend({
-	defaults : {"value" : 1, "event": ""}, //value will represent the date of a month
+	defaults : {"value" : 0, "event": ""}, //value will represent the date of a month
 	initialize : function(){
 		this.fetch();
 	},
@@ -65,29 +65,27 @@ var DayCollectionView = Backbone.View.extend({
 
 		this.collection.add({id: idCount});
 		idCount++;
+		console.log(idCount);
 		console.log('add_day_model');
 	},
 	add_day_view : function(new_model){
 		// console.log('add day view');
-		// console.log(this.collection.models[0]);
+		console.log(this.collection.models[0].length);
 		// console.log(this.collection.models[0].get("value"));
-		// console.log(this.collection.models.length)
 		// this.collection.create({id : idCount});
-		// idCount= idCount + 1;
-		var day_value = this.collection.models[this.collection.models.length-1].get("value");
-		// if(this.collection.models.length===2){
-		// 	console.log("collection is 2");
-		// }
+		// idCount++;
+		console.log(this.collection.models.length);
+		var default_day_value = this.collection.models[this.collection.models.length-2].get("value");
+		var day_value = default_day_value + 1;
 		// default_day_value+= 1;
 		console.log(day_value);
-		// day_value++;
 		new_model.set("value", day_value);
 		// new_model.set("value", idCount); //this will create an incrementing list, but when page is refreshed, they change values
 		var view = new DayView({model: new_model});
 		view.render(); //if this line is commented out, then only one get is called.  The number for value is the same, but changes when you refresh the page
 		$("#calendarDiv").append(view.$el);
 		// console.log("new model added");
-		day_value++;
+		// day_value++;
 	}
 })
 
