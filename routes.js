06edfd4018de6decb03770fc14e61b40e57cc7ec
nathan-j-brenner <nathan.j.brenner@gmail.com>
2015-06-29 10:44:25 -0700

    routes demo from dan

---
+++ routes.js
@@ -0,0 +1,65 @@
+_.templateSettings = {
+	interpolate: /\{\{(.+?)\}\}/g
+};
+
+var template = _.template(
+	'Click <a href="{{url}}">here</a> for the {{name}} view!');
+
+//var href = template({url:'/', name: 'other'});
+
+var OneView = Backbone.View.extend({
+	el: '#myApp';
+
+	initialize: function(){
+		this.render();
+		this.$el.appendTo('#myApp');
+		$('#myApp').html(this.$el);
+	},
+	render: function(data){
+		var name = data? data : 'other';
+		ths.$el.html(template({url:'#other', name: name}));
+	}
+});
+
+var OtherView = Backbone.View.extend({
+	el: '#myApp',
+	initialize: function(){
+		this.render();
+		//this.$el.appendTo('#myApp');
+		//$('#myApp').html(this.$el);
+	},
+	render: function(data){
+		var name = data || 'one';
+		this.$el.html(template({url: '#one', name: name})))
+	}
+});
+
+var MyRouter = Backbone.Router.extend({
+	routes: {
+		'': 'one',
+		'one': 'one',
+		'other': 'other',
+		'special(/:data)': 'display'
+	},
+	display: function(data){
+		this.view.render(data);
+	},
+	one: function(){
+		//if(this.view)
+		//	this.view.remove();
+		this.view = new OneView();
+	},
+	other: function(){
+		//if(this.view)
+		//	this.view.remove();
+		this.view = new OtherView();
+	}
+});
+
+
+var view;
+$(function(){
+//	view = new RedirectView();
+	var router = new MyRouter();
+	Backbone.history.start();
+});
