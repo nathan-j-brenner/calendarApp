d84b4e186e01ab025e0a6564567009f0d9f65554
nathan-j-brenner <nathan.j.brenner@gmail.com>
2015-07-12 23:11:51 -0700

    julian calendar works

--- find_month.js
+++ find_month.js
@@ -1,21 +1,95 @@
-var month_list = ["January", "February", "March", "April", "May", "June", "July", "August", "October", "November", "December"];
-var month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-
-var month = 0;
-
-function find_month(day){
-  if(day<31){
-    for(var i = 0; i<month_days.length; i++){
-      if(day>month_days[i]){ //if day 31 is more than the number of days in january
-			  day -= month_days[i]; // day = 31 - 31 = 0
-		  	month+=1;
-		  }
-	  }
-  }
-	for(var i = 0; i<month_list.length; i++){
-		if(month===i){
-			month = month_list[i];
-		}
+var year_2015 = {
+	jan : {
+		days : 31,
+		dates : "1-31"
+	},
+	feb : {
+		days : 28,
+		dates : "32-59"
+	},
+	march : {
+		days : 31,
+		dates : "60-90"
+	},
+	april : {
+		days : 30,
+		dates : "91-120"
 	}
-	console.log("month: " + month + ", day: " + day);  
+	may : {
+		days : 31,
+		dates : "121-151"
+	},
+	june : {
+		days : 30,
+		dates : "152-181"
+	},
+	july : {
+		days : 31,
+		dates : "182-212"
+	},
+	august : {
+		days : 31,
+		dates : "213-243"
+	},
+	sept : {
+		days : 30,
+		dates : "244-273"
+	},
+	oct : {
+		days : 31,
+		dates : "274-304"
+	},
+	nov : {
+		days : 30,
+		dates : "305-334"
+	},
+	dec : {
+		days : 31,
+		dates : "335-365"
+	},
+}
+
+function find_date(day_num){
+	var month = "";
+	var date = "";
+	if(day_num<32){
+		month = "January";
+		date = day_num;
+	} else if (day_num<60){
+		month = "February";
+		date = day_num - 31;// day_number - last months days
+	} else if(day_num<91){
+		month = "March";
+		date = day_num - 59;
+	} else if(day_num<121){
+		month = "April";
+		date = day_num - 90;
+	}else if(day_num<152){
+		month = "May";
+		date = day_num - 120;
+	}else if(day_num<182){
+		month = "June";
+		date = day_num - 151;
+	}else if(day_num<213){
+		month = "July";
+		date = day_num - 181;
+	}else if(day_num<244){
+		month = "August";
+		date = day_num - 212;
+	}else if(day_num<274){
+		month = "September";
+		date = day_num - 243;
+	}else if(day_num<305){
+		month = "October";
+		date = day_num - 273;
+	}else if(day_num<335){
+		month = "November";
+		date = day_num - 304;
+	} else if (day_num<365){
+		month = "December";
+		date = day_num - 334;
+	}
+  var day_num_date = "Today is " + month + " " + date + ", 2015.";
+  var err = "That day is not in 2015";
+	console.log(err || day_num_date);
 }
